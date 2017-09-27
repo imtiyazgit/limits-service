ed447a8563e7e0d11d85a7521c031a7a908fa7e0
imtiyazgit <imtiyaz.208@gmail.com>
2017-09-26 21:39:10 -0700

    first

---
+++ src/main/java/com/imtiyaz/microservices/limitsservice/bean/LimitConfiguration.java
@@ -0,0 +1,23 @@
+package com.imtiyaz.microservices.limitsservice.bean;
+
+public class LimitConfiguration {
+	private int maximum;
+	private int minimum;
+	
+	public LimitConfiguration() {}
+	
+	public LimitConfiguration(int maximum, int minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+	
+	public int getMaximum() {
+		return maximum;
+	}
+	public int getMinimum() {
+		return minimum;
+	}
+	
+	
+}
